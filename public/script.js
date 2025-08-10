f6c6e3ea3be282aa3a3773e960a894518b79cbfc
2313Bernice <b2029bvlorenzo@pshs.edu.ph>
2025-08-11 07:49:31 +0800

    1NG Exercise

---
+++ public/script.js
@@ -0,0 +1,7 @@
+let age = 14;
+let maxAge = 100;
+let numPerDay = 1; 
+
+totalRequired = (numPerDay * 365) * (maxAge - age);
+
+document.write(`You will need ${totalRequired} cups of coffee to last you until the ripe old age of ${maxAge}`);
