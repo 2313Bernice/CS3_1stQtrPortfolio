0eb2c6eabb5d5723b930b4b91ad735ae9bdfe98b
2313Bernice <b2029bvlorenzo@pshs.edu.ph>
2025-08-11 08:48:15 +0800

    1NG Exercise

--- public/script.js
+++ public/script.js
@@ -1,7 +1,14 @@
-let age = 14;
-let maxAge = 100;
-let numPerDay = 1; 
+let age = prompt("Enter your age: ", 1);
+let maxAge = prompt("You will live until the ripe old age of: ", 100);
+let numPerDay = prompt("Number of snacks you eat daily: ", 1 ); 
 
-totalRequired = (numPerDay * 365) * (maxAge - age);
+let sure = confirm("Are you veryyyy sure? Baka mali yung input mo...what if mali yung natype mo and naging 0 yung result? You want to see the answer na ba? Sorry na :(");
 
-document.write(`You will need ${totalRequired} cups of coffee to last you until the ripe old age of ${maxAge}`);
+if(sure){
+    let totalRequired = (Number(numPerDay) * 365) * (Number(maxAge) - age);
+    document.write(`You will need ${totalRequired} cups of coffee to last you until the ripe old age of ${maxAge}`);
+}
+else{
+    window.alert("Why would you leave :(...the answer was a click away!");
+}
+
